f1e85054cf26fadb12314b55261d22603657a80b
AlbertoMr1 <75633353+AlbertoMr1@users.noreply.github.com>
2023-09-28 14:47:35 +0000

    actualizacion

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import inicio from "./routes/inicio_router.js";
 import credenciales_router from "./routes/credenciales_router.js";
+import router_Hotel from "./routes/hotel_router.js";
 import db from "./config/db.js";
+
 //Crear la aplicación
 const app = express();
+
 //accesos a los datos del formulario
 app.use(express.urlencoded({extended:true}))
 
@@ -18,11 +21,15 @@ try {
 //pug
 app.set("view engine", "pug");
 app.set("views", "./views");
+
 //carpeta publica
 app.use(express.static("public"));
+
 //routing
 app.use("/", inicio);
 app.use("/cred", credenciales_router);
+app.use("/hotel", router_Hotel);
+
 //definiendo el puerto
 const port = 2800;
 app.listen(port, () => {
---
+++ models/Usuario.js
@@ -0,0 +1,21 @@
+import {DataTypes} from 'sequelize';
+import db from '../config/db.js';
+
+const Usuario= db.define('usuario',{
+    nombre:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    correo:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    password:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    confirmar:DataTypes.BOOLEAN
+
+})
+
+export default Usuario;
